a1e525d86b530d5152565974a5bed154993bbf4a
pacificr <este6604@pacificu.edu>
2019-10-22 17:39:44 -0400

    Parsing for fun

--- src/UVMHSMain.hs
+++ src/UVMHSMain.hs
@@ -5,6 +5,11 @@ import UVMHS
 
 import qualified UVMHSContrib.Lang.Arith as Arith
 import qualified UVMHSContrib.Lang.SExp as SExp
+import qualified UVMHSContrib.Lang.Fun as Fun
 
 main ∷ IO ()
-main = out "¯\\_﹙ツ﹚_/¯"
+main = do
+  Fun.testParserSuccess
+  --SExp.testSExpTokenizerFailure1
+  --Arith.testParserFailure1
+  --Arith.testParserFailure2
